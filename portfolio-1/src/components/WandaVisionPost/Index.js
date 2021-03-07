73f61ccea3b704168cb86485fd6a6019224b005d
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 18:10:40 -0500

    March 7, 2021 6:10 PM

--- portfolio-1/src/components/WandaVisionPost/Index.js
+++ portfolio-1/src/components/WandaVisionPost/Index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import LikeButton from './components/LikeButton';
 
 export default class WandaVisionPost extends React.Component {
     render() {
         return(
             <div>
                 <h1>WandaVision was so good!!!!</h1>
+                <LikeButton></LikeButton>
             </div>
         )
     }
