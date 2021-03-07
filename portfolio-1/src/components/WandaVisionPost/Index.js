02b8fd735d2cedfbf06c6aebfd5aea79e35ab00c
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 17:48:32 -0500

    March 7, 2021 5:48 PM

--- portfolio-1/src/components/WandaVisionPost/Index.js
+++ portfolio-1/src/components/WandaVisionPost/Index.js
@@ -1,3 +1,11 @@
-function hello() {
-    return "WandaVision was so good!!!";
+import React from 'react'
+
+export default class WandaVisionPost extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>FalconAndWinterSoldierPost</h1>
+            </div>
+        )
+    }
 }
