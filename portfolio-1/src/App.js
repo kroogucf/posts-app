a1a1ef1c6f01244648cf135505420c70a401481b
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 17:28:06 -0500

    March 7, 2021 5:28 PM

--- portfolio-1/src/App.js
+++ portfolio-1/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class PostScreen extends React.Component{
+  constructor(props)
+  super(props)
+  this.state = {screen:"NONE"}
+  }
+
+SetScreen(name) {
+  this.setState({
+    screen: name
+  })
 }
 
-export default App;
+  render() {
+    if (this.state.screen === "NONE") {
+      return (
+        <div>
+          <h1>Select Post</h1>
+          <ul style></ul>
+        </div>
+      )
+    }
+  }
+
+export default class App extends React.Component
