78327d936b6bb05b9e99af1da7b503fa20359cc9
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 22:21:18 -0500

    March 7, 2021 10:21 PM

--- portfolio-1/src/components/WandaVisionPost/Index.js
+++ portfolio-1/src/components/WandaVisionPost/Index.js
@@ -5,7 +5,14 @@ export default class WandaVisionPost extends React.Component {
     render() {
         return(
             <div>
-                <h1>WandaVision was so good!!!!</h1>
+                <div>
+                    <h1>WandaVision was so good!!!!</h1>
+                </div>
+                <div class="postbuttons">
+                    <p>
+                        Where do I even start.... How about, I want more. What an absolutely incredible project from Marvel! Elizabeth Olsen, Paul Bettany, and Kathryn Hahn were so incredible in this series. I will admit after the first couple episodes I was like, 'what is going on?' I was skepitcal at first but very glad I stuck through it. The twists and turns of each episode and wonder where it was going, to having questions be answered about almost everything I had a question about. But in typical Marvel fashion they left it open ended enough so we are left with wanting more and still have questions. I really hope Agatha makes a return because the way Kathryn Hahn portrayed that character was great. Marvel needs more of this character based story driven content because in my opinion this could rival for one of the best, if not the best, Marvel MCU project yet. Cannot wait for Doctor-Strange Multiverse of Madness!
+                    </p>
+                </div>
             </div>
         )
     }
