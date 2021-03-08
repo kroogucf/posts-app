bd7422390588240ab9c516d993aab4c4f77e114e
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 22:03:18 -0500

    March 7, 2021 10:03 PM

--- portfolio-1/src/components/FalconAndWinterSoldierArticle/index.js
+++ portfolio-1/src/components/FalconAndWinterSoldierArticle/index.js
@@ -6,8 +6,8 @@ export default class FalconAndWinterSoldierPost extends React.Component {
     constructor(props) {
         super(props)
         this.post = {
-            name: "Guide to Proper Cat-girl Care",
-            article: "As previously mentioned, your Cat-girl is a remarkably neat and clean creature, but even that being the case, she will require regular bathing. If she is a sedentary breed she may only require a weekly bath. More active breeds that energetically play and sweat may require almost daily bathing. Again, we cannot stress enough that you use approved bathing products. Cat-girls have no real body hair except for the head/ears and tail. Thus their skin is vulnerable to strong soaps which may cause itch and painful irritation.",
+            name: "What I want from Falcon and the Winter Soldier",
+            article: "I want action",
         }
         this.state = {
             reported: false,
