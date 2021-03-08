61c4e2556b31a82152e93187fd5a95e4d6ae06af
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 22:12:43 -0500

    March 7, 2021 10:12 PM

--- portfolio-1/src/components/FalconAndWinterSoldierArticle/index.js
+++ portfolio-1/src/components/FalconAndWinterSoldierArticle/index.js
@@ -7,7 +7,7 @@ export default class FalconAndWinterSoldierPost extends React.Component {
         super(props)
         this.post = {
             name: "What I want from Falcon and the Winter Soldier",
-            article: "Going into Falcon and the Winter Soldier I thought my opinion had been swayed toward normal 'Marvel action movie. I thought this because of the pace that WandaVision had set being very different from normal Marvel projects and much slower story based. But now that WandaVision is over, I want more of that type of story. There was so much character development and twists and turns througout the series, I would love to see that in Falcon and the Winter Soldier. While I do love my blow stuff up, out there typical Marvel projects, I feel like that could be saved more for the movie side of things.",
+            article: "Going into Falcon and the Winter Soldier I thought my opinion had been swayed toward normal 'Marvel action movie' after the first couple episodes of WandaVision. I thought this because of the pace that WandaVision had set being very different from normal Marvel projects and much slower and character driven story based. But now that WandaVision is over, I want more of that type of story. There was so much character development and twists and turns throughout the series, I would love to see that in Falcon and the Winter Soldier. While I do love my blow stuff up, out there typical Marvel projects, I feel like that could be saved more for the movie side of things especially after having seen WandaVision now.",
         }
         this.state = {
             reported: false,
