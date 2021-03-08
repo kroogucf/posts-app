fe682177404b2c59107328dfffe7ce9e858bf375
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 21:18:54 -0500

    March 7, 2021 9:18 PM

--- portfolio-1/src/components/ReportButton/index.js
+++ portfolio-1/src/components/ReportButton/index.js
@@ -4,11 +4,12 @@ export default class ReportButton extends React.Component {
 
     constructor(props) {
         super(props)
+        this.props.buttonTitle = "Report Post"
     }
 
     render() {
         return(
-            <button onClick={this.props.callback}><b>Report Post</b></button>            
+            <button onClick={this.props.callback}><b>{this.props.buttonTitle}</b></button>            
         )
     }
 
