c478985e19cbda2ae059c5d249ba35383a5bc077
kroogucf <dylankrueger@Knights.ucf.edu>
2021-03-07 21:06:34 -0500

    March 7, 2021 9:06 PM

--- portfolio-1/src/components/ReportPost/index.js
+++ portfolio-1/src/components/ReportPost/index.js
@@ -6,11 +6,18 @@ export default class ReportPost extends React.Component {
         super(props)
         this.post = {
             name: "Guide to Proper Cat-girl Care",
-            article: "As previously mentioned, your Cat-girl is a remarkably neat and clean creature, but even that being the case, she will require regular bathing. If she is a sedentary breed she may only require a weekly bath. More active brees that energetically play and sweat may require almost daily bathing. Again, we cannot stress enough that you use S.P.C.G.A. approved bathing products. Cat-girls have no real body hair except for the head/ears and tail. Thus their skin is vulnerable to strong soaps which may cause itch and painful irritation.",
+            article: "As previously mentioned, your Cat-girl is a remarkably neat and clean creature, but even that being the case, she will require regular bathing. If she is a sedentary breed she may only require a weekly bath. More active breeds that energetically play and sweat may require almost daily bathing. Again, we cannot stress enough that you use approved bathing products. Cat-girls have no real body hair except for the head/ears and tail. Thus their skin is vulnerable to strong soaps which may cause itch and painful irritation.",
         }
         this.state = {
             reported: false,
         };
+    }
+
+    reportArticle() {
+        const currentState = this.state
+        let newState = currentState
+        newState.reported = true
+        this.setState(newState)
     }
 
     render() {
@@ -24,7 +31,8 @@ export default class ReportPost extends React.Component {
         return (
                 <div>
                     <h1>{this.post.name}</h1>
-                    <p>{this/this.post.article}</p>
+                    <p>{this.post.article}</p>
+                    <button onClick={this.reportArticle}><b>Report Post</b></button>
                 </div>
                 )
         }
